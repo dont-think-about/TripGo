77dd1b93f966c1d11cfc52758dce0ff67a89e54e
minjong kim <alswhddkdl@gmail.com>
2023-10-17 01:49:46 +0900

    [FEAT]: 캘린터 뷰 기본 세팅

--- app/src/main/java/com/nbcamp/tripgo/view/calendar/CalendarFragment.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/calendar/CalendarFragment.kt
@@ -5,16 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.nbcamp.tripgo.R
+import com.nbcamp.tripgo.databinding.FragmentCalendarBinding
+import com.nbcamp.tripgo.util.calendar.SaturdayDecorator
+import com.nbcamp.tripgo.util.calendar.SundayDecorator
+import java.util.Calendar
 
 class CalendarFragment : Fragment() {
+    private var _binding: FragmentCalendarBinding? = null
+    private val binding: FragmentCalendarBinding
+        get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    ): View {
+        _binding = FragmentCalendarBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        calendarMainView.run {
+            val month = Calendar.getInstance().get(Calendar.MONTH)
+            addDecorators(SaturdayDecorator(month, 1), SundayDecorator(month, 1))
+            setOnMonthChangedListener { _, date ->
+                removeDecorators()
+                invalidateDecorators()
+                addDecorators(SaturdayDecorator(date.month, 0), SundayDecorator(date.month, 0))
+            }
+        }
+
+    }
+
 
     companion object {
         fun newInstance() = CalendarFragment()
