91fb73ee195ebab908c09f1da4caa2ae390decb7
minjong kim <alswhddkdl@gmail.com>
2023-10-18 03:15:50 +0900

    [FEAT]: 유틸리티 생성

--- app/src/main/java/com/nbcamp/tripgo/util/TourTheme.kt
+++ app/src/main/java/com/nbcamp/tripgo/util/TourTheme.kt
@@ -4,5 +4,8 @@ enum class TourTheme(val themeId: Int) {
     FAMILY(0),
     HEALING(1),
     CAMPING(2),
-    TASTY(3)
+    TASTY(3),
+    POPULAR(4),
+    NEARBY(5),
+    SEARCH(-1)
 }
--- app/src/main/java/com/nbcamp/tripgo/util/URL.kt
+++ app/src/main/java/com/nbcamp/tripgo/util/URL.kt
@@ -4,4 +4,5 @@ object URL {
     const val SK_OPEN_API_URL = ""
     const val KAKAO_API_URL = ""
     const val TOUR_API_URL = "http://apis.data.go.kr/B551011/KorService1/"
+    const val DEFAULT_TOUR_API_URL = "http://apis.data.go.kr/B551011/"
 }
