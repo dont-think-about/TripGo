6da3ef925390ce7b5d5916e89f035159bf38e233
minjong kim <alswhddkdl@gmail.com>
2023-10-18 15:56:52 +0900

    [FEAT]: 검색 결과 수신 객체 생성

---
+++ app/src/main/java/com/nbcamp/tripgo/data/model/keywords/KeywordItem.kt
@@ -0,0 +1,47 @@
+package com.nbcamp.tripgo.data.model.keywords
+
+
+import com.google.gson.annotations.SerializedName
+
+data class KeywordItem(
+    @SerializedName("addr1")
+    val addr1: String,
+    @SerializedName("addr2")
+    val addr2: String,
+    @SerializedName("areacode")
+    val areacode: String,
+    @SerializedName("booktour")
+    val booktour: String,
+    @SerializedName("cat1")
+    val cat1: String,
+    @SerializedName("cat2")
+    val cat2: String,
+    @SerializedName("cat3")
+    val cat3: String,
+    @SerializedName("contentid")
+    val contentid: String,
+    @SerializedName("contenttypeid")
+    val contenttypeid: String,
+    @SerializedName("cpyrhtDivCd")
+    val cpyrhtDivCd: String,
+    @SerializedName("createdtime")
+    val createdtime: String,
+    @SerializedName("firstimage")
+    val firstimage: String,
+    @SerializedName("firstimage2")
+    val firstimage2: String,
+    @SerializedName("mapx")
+    val mapx: String,
+    @SerializedName("mapy")
+    val mapy: String,
+    @SerializedName("mlevel")
+    val mlevel: String,
+    @SerializedName("modifiedtime")
+    val modifiedtime: String,
+    @SerializedName("sigungucode")
+    val sigungucode: String,
+    @SerializedName("tel")
+    val tel: String,
+    @SerializedName("title")
+    val title: String
+)
