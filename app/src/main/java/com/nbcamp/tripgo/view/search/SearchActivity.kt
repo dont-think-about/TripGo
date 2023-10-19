6a7e88ffa4902a1278691b2c33306664ae5ff852
sunho_jeong <sunho512@gmail.com>
2023-10-19 20:12:23 +0900

    [Feat] 어트렉션 프래그먼트에서 검색결과 값 보내기

--- app/src/main/java/com/nbcamp/tripgo/view/search/SearchActivity.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/search/SearchActivity.kt
@@ -1,37 +1,58 @@
 package com.nbcamp.tripgo.view.search
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.nbcamp.tripgo.R
+import com.nbcamp.tripgo.data.repository.mapper.WeatherType
+import com.nbcamp.tripgo.data.repository.model.KeywordSearchEntity
+import com.nbcamp.tripgo.data.service.RetrofitModule
 import com.nbcamp.tripgo.databinding.ActivitySearchBinding
 import com.nbcamp.tripgo.view.search.adapters.ViewPagerAdapter
+import com.nbcamp.tripgo.view.search.attaction.AttractionsFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-
-
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(), AttractionsFragment.OnSearchListener {
 
     lateinit var mViewPagerAdapter: ViewPagerAdapter
-
     private lateinit var binding: ActivitySearchBinding
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         mViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
         binding.searchViewpager.adapter = mViewPagerAdapter
-
-        //탭 레이아웃을 => 뷰페이저와 연결.
         binding.searchTabLayout.setupWithViewPager(binding.searchViewpager)
+        recyclerView = findViewById(R.id.search_rank_recyclerview) // 리사이클러뷰 연결
 
+        val searchBackImageView = findViewById<ImageView>(R.id.search_back)
+        searchBackImageView.setOnClickListener {
+            finish() // 뒤로 가기 버튼을 클릭하면 Activity 종료
+        }
+    }
+    override fun onSearch(text: String) {
+        getSearchResults(text)
+    }
+
+    private fun getSearchResults(query: String) {
+        val arraylist = arrayListOf<KeywordSearchEntity>()
+        CoroutineScope(Dispatchers.IO).launch {
+//            val response = RetrofitModule.createTourApiService(query)
+//            response.body()?.let { searchResults ->
+//                searchResults.items.forEach { item ->
+//                    arraylist.add(
+//                        KeywordSearchEntity(
+//                        title = item.bo
+//                        )
+//                    )
+//                }
+//            }
+        }
     }
 }
-
-
-
-
-
-
-
 
--- app/src/main/java/com/nbcamp/tripgo/view/search/attaction/AttractionsFragment.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/search/attaction/AttractionsFragment.kt
@@ -1,11 +1,15 @@
 package com.nbcamp.tripgo.view.search.attaction
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.nbcamp.tripgo.R
@@ -13,16 +17,31 @@ import com.nbcamp.tripgo.databinding.FragmentSearchAttractionsBinding
 
 class AttractionsFragment : Fragment() {
 
-
+    private lateinit var editText: EditText
+    private lateinit var searchButton: ImageView
     private var _binding: FragmentSearchAttractionsBinding? = null
     private val binding get() = _binding!!
+
+    // 인터페이스를 정의합니다.
+    interface OnSearchListener {
+        fun onSearch(searchText: String)
+    }
+
+    private var searchListener: OnSearchListener? = null
+
+    // 인터페이스 리스너를 연결합니다.
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnSearchListener) {
+            searchListener = context
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentSearchAttractionsBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -30,31 +49,42 @@ class AttractionsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        editText = binding.attractionsSearchEdit
+        searchButton = binding.attractionsSearchOk  // 'searchButton'이 OK 버튼이라고 가정합니다.
 
+        // EditText의 텍스트 변경을 감지하는 TextWatcher를 추가합니다.
         binding.attractionsSearchEdit.addTextChangedListener(object : TextWatcher {
-
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
+                // 텍스트가 비어 있지 않으면 'attractionsSearchCloseImage'를 보이도록 설정, 비어 있으면 숨김.
                 binding.attractionsSearchCloseImage.visibility =
-
                     if (s?.isNotEmpty() == true) View.VISIBLE else View.GONE
             }
             override fun afterTextChanged(s: Editable?) {}
-        })  // 텍스트가 비어 있지 않으면 x 버튼 보이게 하고, 비어 있으면 x 버튼이 안 보이게 함.
+        })
 
+        // 검색 버튼(ImageView) 클릭 시 동작 설정
+        searchButton.setOnClickListener {
+            val searchText = editText.text.toString()
+            searchListener?.onSearch(searchText) // 검색 결과를 액티비티로 전달
+            hideKeyboard() // 키보드 숨김
+        }
 
-
-
+        // 'attractionsSearchCloseImage' 클릭 시 EditText 내용을 지웁니다.
         binding.attractionsSearchCloseImage.setOnClickListener {
-            binding.attractionsSearchEdit.text?.clear()
-        }   // x 버튼 클릭시 전부 지움.
+            editText.text?.clear()
+        }
 
-
+        // 'edit_closeBtn' 색상 설정
         val closeButtonColor = ContextCompat.getColor(requireContext(), R.color.edit_closeBtn)
         binding.attractionsSearchCloseImage.setColorFilter(closeButtonColor)
-    }  // edit_closeBtn 색상 변경
+    }
+
+    // 키보드를 숨기는 메서드
+    private fun hideKeyboard() {
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(editText.windowToken, 0)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
