250f4d69b74e41469d191a0129e51919478a6c0e
minjong kim <alswhddkdl@gmail.com>
2023-10-20 02:54:10 +0900

    [FEAT]: 캘린더 날짜 배경 decorator 생성

---
+++ app/src/main/java/com/nbcamp/tripgo/util/calendar/SelectedDayDecorator.kt
@@ -0,0 +1,21 @@
+package com.nbcamp.tripgo.util.calendar
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.nbcamp.tripgo.R
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+
+class SelectedDayDecorator(context: Context) : DayViewDecorator {
+    private val drawable = ContextCompat.getDrawable(context, R.drawable.calendar_selector)
+
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        return true
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.setSelectionDrawable(drawable!!)
+        view.setDaysDisabled(true)
+    }
+}
