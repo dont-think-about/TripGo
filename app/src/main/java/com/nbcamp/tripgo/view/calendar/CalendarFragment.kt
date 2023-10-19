46b54f663c9f00c416200e20866b8910f0209e66
minjong kim <alswhddkdl@gmail.com>
2023-10-20 00:09:52 +0900

    [FEAT]: 가져온 데이터를 리사이클러 뷰에 바인딩

--- app/src/main/java/com/nbcamp/tripgo/view/calendar/CalendarFragment.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/calendar/CalendarFragment.kt
@@ -4,16 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.nbcamp.tripgo.R
 import com.nbcamp.tripgo.databinding.FragmentCalendarBinding
 import com.nbcamp.tripgo.util.calendar.SaturdayDecorator
 import com.nbcamp.tripgo.util.calendar.SundayDecorator
+import com.nbcamp.tripgo.util.extension.ContextExtension.toast
+import com.nbcamp.tripgo.view.calendar.uistate.CalendarScheduleUiState
 import java.util.Calendar
 
 class CalendarFragment : Fragment() {
     private var _binding: FragmentCalendarBinding? = null
     private val binding: FragmentCalendarBinding
         get() = _binding!!
+    private val calendarViewModel: CalendarViewModel by viewModels {
+        CalendarViewModelFactory(
+            requireActivity()
+        )
+    }
+    private val scheduleListAdapter by lazy {
+        ScheduleListAdapter {
+
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +43,43 @@ class CalendarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        initViewModel()
+    }
+
+    private fun initViewModel() = with(calendarViewModel) {
+        getLoginStatus()
+        with(binding) {
+            loginStatus.observe(viewLifecycleOwner) { state ->
+                when (state) {
+                    true -> {
+                        getSchedulesFromFireStore()
+                    }
+
+                    false -> {
+                        calendarNoticeTextView.text = getString(R.string.log_in_and_submit_review)
+                    }
+                }
+            }
+            myScheduleState.observe(viewLifecycleOwner) { state ->
+                if (state == CalendarScheduleUiState.error(state.message)) {
+                    state.message?.let {
+                        requireActivity().toast(it)
+                    }
+                    return@observe
+                }
+                if (state.data?.isEmpty() == true) {
+                    calendarNoticeTextView.isVisible = true
+                    calendarNoticeTextView.text = getString(R.string.add_schedule_and_submit_review)
+                }
+                calendarNoticeTextView.isVisible = false
+                calendarProgressBar.isVisible = state.isLoading
+                scheduleListAdapter.submitList(state.data)
+            }
+        }
+    }
+
+    private fun getSchedulesFromFireStore() {
+        calendarViewModel.getSchedulesFromFireStoreDatabase()
     }
 
     private fun initViews() = with(binding) {
@@ -39,8 +92,15 @@ class CalendarFragment : Fragment() {
                 addDecorators(SaturdayDecorator(date.month, 0), SundayDecorator(date.month, 0))
             }
         }
+        calendarScheduleRecyclerView.run {
+            adapter = scheduleListAdapter
+        }
     }
 
+    override fun onResume() {
+        super.onResume()
+        calendarViewModel.getLoginStatus()
+    }
 
     companion object {
         fun newInstance() = CalendarFragment()
