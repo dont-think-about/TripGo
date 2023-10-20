05870e4ee5928efedc7e946ddc5129461e0c3617
minjong kim <alswhddkdl@gmail.com>
2023-10-20 15:53:56 +0900

    [DOCS]: 관련 Repository 주석 추가

--- app/src/main/java/com/nbcamp/tripgo/view/calendar/CalendarRepository.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/calendar/CalendarRepository.kt
@@ -3,6 +3,15 @@ package com.nbcamp.tripgo.view.calendar
 import com.nbcamp.tripgo.data.repository.model.CalendarEntity
 
 interface CalendarRepository {
+
+    /**
+     * @return 로그인 한 사용자의 정보 (firebase or KaKao)
+     */
     fun getCurrentUser(): Any?
+
+    /**
+     *  @param email 로그인한 사용자의 이메일
+     *  @return 사용자의 일정 목록
+     */
     suspend fun getMySchedules(email: String): List<CalendarEntity>
 }
