d22c2d34aab2f079c2d624d4ddf5a8d5fb3f9baf
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-10-24 03:47:08 +0900

    [FEAT]: Firebase를 사용하여 회원가입 기본 로직 작성 (닉네임, 이미지(초기 null) 값 저장

--- app/src/main/java/com/nbcamp/tripgo/view/signup/SignUpActivity.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/signup/SignUpActivity.kt
@@ -1,12 +1,178 @@
 package com.nbcamp.tripgo.view.signup
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.nbcamp.tripgo.R
+import com.nbcamp.tripgo.databinding.ActivitySignUpBinding
+import com.nbcamp.tripgo.view.login.LogInActivity
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+    private val signUpViewModel: SignUpViewModel by viewModels()
+
+    private var emailCheck = false
+    private var passwordCheck = false
+    private var passwordRepeatCheck = false
+    private var nicknameCheck = false
+    private var agreeCheck = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        binding.viewModel = signUpViewModel
+        binding.lifecycleOwner = this
+        initViewModel()
+        initView()
+        editTextMemberInformation()
     }
+
+
+    private fun initView() = binding.apply {
+        signUpFullAgreementCheckBox.setOnClickListener {
+            if (signUpFullAgreementCheckBox.isChecked) {
+                signUpAgeLimitCheckBox.isChecked = true
+                signUpAppAlarmCheckBox.isChecked = true
+                signUpPrivacyPolicyCheckBox.isChecked = true
+                signUpTermsOfUseCheckBox.isChecked = true
+                signUpEssentialAgreementErrorTextView.visibility = View.GONE
+                signUpTermsAndConditionsAgreementLayout.setBackgroundResource(R.drawable.background_edit_text)
+                agreeCheck = true
+                checkBoxAllChecked()
+            } else {
+                signUpAgeLimitCheckBox.isChecked = false
+                signUpAppAlarmCheckBox.isChecked = false
+                signUpPrivacyPolicyCheckBox.isChecked = false
+                signUpTermsOfUseCheckBox.isChecked = false
+                signUpEssentialAgreementErrorTextView.visibility = View.VISIBLE
+                signUpTermsAndConditionsAgreementLayout.setBackgroundResource(R.drawable.background_edit_text_error)
+                agreeCheck = false
+//                binding.signUpSignUpCompleteButton.isEnabled = false
+            }
+        }
+
+        signUpAgeLimitCheckBox.setOnClickListener {
+            checkBoxEssentialChecked()
+        }
+
+        signUpPrivacyPolicyCheckBox.setOnClickListener {
+            checkBoxEssentialChecked()
+        }
+        signUpTermsOfUseCheckBox.setOnClickListener {
+            checkBoxEssentialChecked()
+        }
+    }
+
+    private fun initViewModel() {
+
+        signUpViewModel.signUpButton.observe(this) {
+            if (it) {
+                finish()
+                startActivity(Intent(this, LogInActivity::class.java))
+            }
+        }
+    }
+
+    private fun editTextMemberInformation() = binding.apply {
+
+        val editTexts = arrayOf(
+            signUpEmailEditText,
+            signUpPasswordEditText,
+            signUpCorrectPasswordEditText,
+            signUpNickNameEditText
+        )
+
+        val errorViews = arrayOf(
+            signUpEmailErrorTextView,
+            signUpPasswordErrorTextView,
+            signUpCorrectPasswordErrorTextView,
+            signUpNickNameErrorTextView
+        )
+
+        val bgViews = arrayOf(
+            signUpEmailLayout,
+            signUpPasswordLayout,
+            signUpCorrectPasswordLayout,
+            signUpNickNameLayout
+        )
+
+        fun EditText.showError() {
+            val index = editTexts.indexOf(this)
+            errorViews[index].visibility = View.VISIBLE
+            bgViews[index].setBackgroundResource(R.drawable.background_edit_text_error)
+
+        }
+
+        fun EditText.hideError() {
+            val index = editTexts.indexOf(this)
+            errorViews[index].visibility = View.GONE
+            bgViews[index].setBackgroundResource(R.drawable.background_edit_text)
+
+        }
+
+        fun EditText.addValidation(validation: (String) -> Boolean) {
+            addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {}
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (validation(text.toString())) {
+                        hideError()
+                    } else {
+                        showError()
+                    }
+                }
+            }
+            )
+        }
+
+        signUpEmailEditText.addValidation { email ->
+            email.matches(Regex("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}\$"))
+        }
+
+        signUpPasswordEditText.addValidation { password ->
+            password.length >= 8
+        }
+
+        signUpCorrectPasswordEditText.addValidation { passwordRepeat ->
+            val password = signUpPasswordEditText.text.toString()
+            passwordRepeat.isEmpty() || passwordRepeat == password
+        }
+
+        signUpNickNameEditText.addValidation { nickname ->
+            nickname.length in 2..15
+        }
+    }
+
+    private fun checkBoxEssentialChecked() = binding.apply {
+
+        if (signUpAgeLimitCheckBox.isChecked && signUpPrivacyPolicyCheckBox.isChecked && signUpTermsOfUseCheckBox.isChecked) {
+            signUpEssentialAgreementErrorTextView.visibility = View.GONE
+            signUpTermsAndConditionsAgreementLayout.setBackgroundResource(R.drawable.background_edit_text)
+            agreeCheck = true
+            checkBoxAllChecked()
+        } else {
+            binding.signUpEssentialAgreementErrorTextView.visibility = View.VISIBLE
+            signUpTermsAndConditionsAgreementLayout.setBackgroundResource(R.drawable.background_edit_text_error)
+            agreeCheck = false
+//            binding.signUpSignUpCompleteButton.isEnabled = false
+        }
+    }
+
+    private fun checkBoxAllChecked() = binding.apply {
+        if (emailCheck && passwordCheck && passwordRepeatCheck && nicknameCheck && agreeCheck) {
+            signUpSignUpCompleteButton.isEnabled = true
+            signUpSignUpCompleteButton.setBackgroundResource(R.color.main)
+        } else {
+//            signUpSignUpCompleteButton.isEnabled = false
+        }
+    }
+
 }
---
+++ app/src/main/java/com/nbcamp/tripgo/view/signup/SignUpViewModel.kt
@@ -0,0 +1,50 @@
+package com.nbcamp.tripgo.view.signup
+
+import android.content.ContentValues
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+
+class SignUpViewModel : ViewModel() {
+    var firebaseAuth: FirebaseAuth = Firebase.auth
+    var fireStore: FirebaseFirestore = Firebase.firestore
+
+    var email: MutableLiveData<String> = MutableLiveData("")
+    var password: MutableLiveData<String> = MutableLiveData("")
+    var nickname: MutableLiveData<String> = MutableLiveData("")
+    var signUpButton: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    fun signUpComplete() {
+        val email = email.value.toString()
+        val password = password.value.toString()
+        val nickname = nickname.value.toString()
+
+        if (email.trim().isNotEmpty() && password.trim().isNotEmpty() && nickname.trim().isNotEmpty()) {
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+
+                    signUpButton.value = true
+
+                    val user = hashMapOf(
+                        "nickname" to nickname,
+                        "profileImage" to null,
+                    )
+
+                    fireStore.collection("users").document(email).set(user)
+
+                    Log.d(ContentValues.TAG, "createUserWithEmail:success")
+
+                } else {
+                    //아이디가 있을 경우에! 로직 추가 ->다음 작업 시 진행
+                    Log.w(ContentValues.TAG, "createUserWithEmail:failure", it.exception)
+                }
+            }
+
+        }
+    }
+}
