cec6db477bd413d8643f0b9ae16022eb688ef7a2
minjong kim <alswhddkdl@gmail.com>
2023-10-26 20:16:03 +0900

    [FEAT]: 스플래시 화면 구현

--- app/src/main/java/com/nbcamp/tripgo/view/SplashActivity.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/SplashActivity.kt
@@ -1,12 +1,36 @@
 package com.nbcamp.tripgo.view
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.nbcamp.tripgo.R
+import com.nbcamp.tripgo.databinding.ActivitySplashBinding
+import com.nbcamp.tripgo.view.main.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
+        val animation = binding.splashLottie
+        animation.run {
+            setAnimation("lottie_5.json")
+            speed = 4f
+            playAnimation()
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_up_enter, R.anim.slide_down_exit)
+            finish()
+        }, 2500)
     }
 }
