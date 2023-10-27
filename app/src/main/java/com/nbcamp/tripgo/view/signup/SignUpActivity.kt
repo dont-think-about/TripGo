eded508c3cd5caed6260b71ecc5066c526f50a75
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-10-27 11:21:37 +0900

    [FEAT]: 회원가입 유효화 검사 + 이메일 인증

--- app/src/main/java/com/nbcamp/tripgo/view/signup/SignUpActivity.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/signup/SignUpActivity.kt
@@ -4,28 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.View
 import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.DataBindingUtil
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
 import com.nbcamp.tripgo.R
 import com.nbcamp.tripgo.databinding.ActivitySignUpBinding
 import com.nbcamp.tripgo.view.login.LogInActivity
-import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySignUpBinding
     private val signUpViewModel: SignUpViewModel by viewModels()
 
-    private var emailCheck = false
-    private var passwordCheck = false
-    private var passwordRepeatCheck = false
-    private var nicknameCheck = false
-    private var agreeCheck = false
+    var agreeCheck = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +30,7 @@ class SignUpActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         initViewModel()
         initView()
-//        editTextMemberInformation()
+        editTextMemberInformation()
     }
 
     private fun initView() = binding.apply {
@@ -47,7 +43,7 @@ class SignUpActivity : AppCompatActivity() {
                 signUpEssentialAgreementErrorTextView.visibility = View.GONE
                 signUpTermsAndConditionsAgreementLayout.setBackgroundResource(R.drawable.background_edit_text)
                 agreeCheck = true
-                checkBoxAllChecked()
+//                checkBoxAllChecked()
             } else {
                 signUpAgeLimitCheckBox.isChecked = false
                 signUpAppAlarmCheckBox.isChecked = false
@@ -56,7 +52,6 @@ class SignUpActivity : AppCompatActivity() {
                 signUpEssentialAgreementErrorTextView.visibility = View.VISIBLE
                 signUpTermsAndConditionsAgreementLayout.setBackgroundResource(R.drawable.background_edit_text_error)
                 agreeCheck = false
-//                binding.signUpSignUpCompleteButton.isEnabled = false
             }
         }
 
@@ -70,6 +65,14 @@ class SignUpActivity : AppCompatActivity() {
         signUpTermsOfUseCheckBox.setOnClickListener {
             checkBoxEssentialChecked()
         }
+
+        signUpEmailAuthButton.setOnClickListener {
+            signUpViewModel.checkEmailDuplication(signUpEmailEditText.text.toString())
+        }
+        signUpNickNameAuthButton.setOnClickListener {
+            signUpViewModel.checkNickNameDuplication(signUpNickNameEditText.text.toString())
+        }
+
     }
 
     private fun initViewModel() {
@@ -80,79 +83,44 @@ class SignUpActivity : AppCompatActivity() {
                 startActivity(Intent(this, LogInActivity::class.java))
             }
         }
-    }
 
-    private fun editTextMemberInformation() = binding.apply {
+        signUpViewModel.isEmailRegistered.observe(this) {
+            if (it == false) {
+                binding.signUpEmailLayout.setBackgroundResource(R.drawable.background_edit_text_error)
+                binding.signUpEmailErrorDuplicationTextView.visibility = View.VISIBLE
 
-        val editTexts = arrayOf(
-            signUpEmailEditText,
-            signUpPasswordEditText,
-            signUpCorrectPasswordEditText,
-            signUpNickNameEditText
-        )
-
-        val errorViews = arrayOf(
-            signUpEmailErrorTextView,
-            signUpPasswordErrorTextView,
-            signUpCorrectPasswordErrorTextView,
-            signUpNickNameErrorTextView
-        )
-
-        val bgViews = arrayOf(
-            signUpEmailLayout,
-            signUpPasswordLayout,
-            signUpCorrectPasswordLayout,
-            signUpNickNameLayout
-        )
-
-        fun EditText.showError() {
-            val index = editTexts.indexOf(this)
-            errorViews[index].visibility = View.VISIBLE
-            bgViews[index].setBackgroundResource(R.drawable.background_edit_text_error)
-
-        }
-
-        fun EditText.hideError() {
-            val index = editTexts.indexOf(this)
-            errorViews[index].visibility = View.GONE
-            bgViews[index].setBackgroundResource(R.drawable.background_edit_text)
-
-        }
-
-        fun EditText.addValidation(validation: (String) -> Boolean) {
-            addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {}
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    if (validation(text.toString())) {
-                        hideError()
-                    } else {
-                        showError()
+                binding.signUpEmailEditText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+                    if (keyCode == KeyEvent.KEYCODE_DEL) {
+                        binding.signUpEmailLayout.setBackgroundResource(R.drawable.background_edit_text)
+                        binding.signUpEmailErrorDuplicationTextView.visibility = View.GONE
                     }
-                }
+                    false
+                })
+            } else {
+                binding.signUpEmailLayout.setBackgroundResource(R.drawable.background_edit_text_correct)
+                binding.signUpEmailErrorDuplicationTextView.visibility = View.GONE
             }
-            )
         }
 
-        signUpEmailEditText.addValidation { email ->
-            email.matches(Regex("[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$"))
+        signUpViewModel.isNickNameRegistered.observe(this) {
+            if (it == false) {
+                binding.signUpNickNameLayout.setBackgroundResource(R.drawable.background_edit_text_error)
+                binding.signUpNickNameErrorTextView.visibility = View.VISIBLE
+                binding.signUpNickNameEditText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+                    if (keyCode == KeyEvent.KEYCODE_DEL) {
+                        binding.signUpNickNameLayout.setBackgroundResource(R.drawable.background_edit_text)
+                        binding.signUpNickNameErrorTextView.visibility = View.GONE
+                    }
+                    false
+                })
 
+            } else {
+                binding.signUpNickNameLayout.setBackgroundResource(R.drawable.background_edit_text_correct)
+                binding.signUpNickNameErrorTextView.visibility = View.GONE
+            }
         }
 
-        signUpPasswordEditText.addValidation { password ->
-            password.matches(
-                Regex("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{8,20}$")
-            )
-        }
 
-        signUpCorrectPasswordEditText.addValidation { passwordRepeat ->
-            val password = signUpPasswordEditText.text.toString()
-            passwordRepeat.isEmpty() || passwordRepeat == password
-        }
-
-        signUpNickNameEditText.addValidation { nickname ->
-            nickname.length in 2..15
-        }
     }
 
     private fun checkBoxEssentialChecked() = binding.apply {
@@ -161,62 +129,85 @@ class SignUpActivity : AppCompatActivity() {
             signUpEssentialAgreementErrorTextView.visibility = View.GONE
             signUpTermsAndConditionsAgreementLayout.setBackgroundResource(R.drawable.background_edit_text)
             agreeCheck = true
-            checkBoxAllChecked()
+//            checkBoxAllChecked()
         } else {
             binding.signUpEssentialAgreementErrorTextView.visibility = View.VISIBLE
             signUpTermsAndConditionsAgreementLayout.setBackgroundResource(R.drawable.background_edit_text_error)
             agreeCheck = false
-//            binding.signUpSignUpCompleteButton.isEnabled = false
         }
     }
 
-    private fun checkBoxAllChecked() = binding.apply {
-        if (emailCheck && passwordCheck && passwordRepeatCheck && nicknameCheck && agreeCheck) {
-            signUpSignUpCompleteButton.isEnabled = true
-            signUpSignUpCompleteButton.setBackgroundResource(R.color.main)
-        } else {
-//            signUpSignUpCompleteButton.isEnabled = false
-        }
+    private fun editTextMemberInformation() = binding.apply {
+
+        signUpEmailEditText.addValidation(
+            validation = {
+                it.matches(
+                    Regex("[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$")
+                )
+            },
+            errorText = {
+                signUpEmailErrorTextView
+            },
+            background = {
+                signUpEmailLayout
+            }
+        )
+
+        signUpPasswordEditText.addValidation(
+            validation = {
+                it.matches(
+                    Regex("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{8,20}$")
+                )
+            },
+            errorText = {
+                signUpPasswordErrorTextView
+            },
+            background = {
+                signUpPasswordLayout
+            }
+        )
+
+        signUpCorrectPasswordEditText.addValidation(
+            validation = {
+                val password = signUpPasswordEditText.text.toString()
+                it.isEmpty() || it == password
+            },
+            errorText = {
+                signUpCorrectPasswordErrorTextView
+            },
+            background = {
+                signUpCorrectPasswordLayout
+            }
+        )
     }
 
-    fun text() {
-        binding.signUpEmailEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
+    private fun EditText.addValidation(
+        validation: (String) -> Boolean,
+        errorText: () -> AppCompatTextView,
+        background: () -> LinearLayout
+    ) {
+        addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-                val ps: Pattern =
-                    Pattern.compile("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
-
-                if (!ps.matcher(binding.signUpEmailEditText.text.toString()).matches()) {
-                    if (binding.signUpEmailEditText.text.toString().isEmpty()) {
-                        binding.signUpEmailErrorTextView.visibility = View.VISIBLE
-                        binding.signUpEmailLayout.setBackgroundResource(R.drawable.background_edit_text_error)
-                        emailCheck = false
-                        binding.signUpSignUpCompleteButton.isEnabled = false
-                    } else {
-                        binding.signUpEmailErrorTextView.visibility = View.VISIBLE
-                        binding.signUpEmailLayout.setBackgroundResource(R.drawable.background_edit_text_error)
-                        emailCheck = false
-                        binding.signUpSignUpCompleteButton.isEnabled = false
-                    }
+                if (validation(text.toString())) {
+                    hideError(errorText, background)
                 } else {
-                    binding.signUpEmailErrorTextView.visibility = View.GONE
-                    binding.signUpEmailLayout.setBackgroundResource(R.drawable.background_edit_text)
-                    emailCheck = true
-                    if (emailCheck and passwordCheck and passwordRepeatCheck and nicknameCheck and agreeCheck) {
-                        binding.signUpSignUpCompleteButton.isEnabled = true
-                        binding.signUpSignUpCompleteButton.setBackgroundResource(R.color.main)
-                    } else {
-                        binding.signUpSignUpCompleteButton.isEnabled = false
-                    }
+                    showError(errorText, background)
                 }
             }
         })
+    }
 
+    private fun showError(errorText: () -> AppCompatTextView, background: () -> LinearLayout) {
+        errorText().visibility = View.VISIBLE
+        background().setBackgroundResource(R.drawable.background_edit_text_error)
+
+    }
+
+    private fun hideError(errorText: () -> AppCompatTextView, background: () -> LinearLayout) {
+        errorText().visibility = View.GONE
+        background().setBackgroundResource(R.drawable.background_edit_text)
     }
 }
