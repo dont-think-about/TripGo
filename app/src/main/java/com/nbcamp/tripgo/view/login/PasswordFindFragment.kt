58030079d18cf9c22de18e9a01fe0224227f2f78
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-10-27 17:29:30 +0900

    [FEAT]: 화면 크기에 맞게 다이얼로그 기초 작업

---
+++ app/src/main/java/com/nbcamp/tripgo/view/login/PasswordFindFragment.kt
@@ -0,0 +1,39 @@
+package com.nbcamp.tripgo.view.login
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.nbcamp.tripgo.R
+import com.nbcamp.tripgo.databinding.DialogPasswordFindBinding
+
+class PasswordFindFragment : DialogFragment() {
+    private var _binding: DialogPasswordFindBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.Dialog)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = DialogPasswordFindBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        return dialog
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
