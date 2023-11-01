a7303555584e869380f30b348830e87e523af032
minjong kim <alswhddkdl@gmail.com>
2023-11-01 12:01:36 +0900

    [FEAT]: BottomSheet을 통한 리뷰 단일 필터링 구현

--- app/src/main/java/com/nbcamp/tripgo/view/review/whole/ReviewViewModel.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/review/whole/ReviewViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nbcamp.tripgo.view.review.whole.ReviewRepository
+import com.nbcamp.tripgo.view.reviewwriting.ReviewWritingModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -14,6 +15,12 @@ class ReviewViewModel(
     private val _reviewUiState: MutableLiveData<ReviewUiState> = MutableLiveData()
     val reviewUiState: LiveData<ReviewUiState>
         get() = _reviewUiState
+
+    private val _filteredList: MutableLiveData<List<ReviewWritingModel>> = MutableLiveData()
+    val filteredList: LiveData<List<ReviewWritingModel>>
+        get() = _filteredList
+
+    private var cachingReviews = listOf<ReviewWritingModel>()
 
     fun getAllReviews() {
         _reviewUiState.value = ReviewUiState.initialize("로딩 중..")
@@ -27,9 +34,29 @@ class ReviewViewModel(
                         false
                     )
                 )
+                cachingReviews = allReview
             }.onFailure {
                 _reviewUiState.postValue(ReviewUiState.error("로딩 실패.."))
             }
         }
+    }
+
+    fun setFilteredReview(category: String, text: CharSequence) {
+        // 필터링 해서 - 빈 값일 때는 원상 복구
+        val filteredReviews = when (text) {
+            "" -> cachingReviews
+            "성별" -> cachingReviews.filter { it.gender == category }
+            "나이" -> cachingReviews.filter { it.generation == category }
+            "동반인" -> cachingReviews.filter { it.companion == category }
+            else -> {
+                cachingReviews.filter {
+                    it.address
+                        .split(" ").first().split("")
+                        .containsAll(category.split(""))
+                }
+            }
+        }
+        // 라이브데이터로 제공
+        _filteredList.value = filteredReviews
     }
 }
--- app/src/main/java/com/nbcamp/tripgo/view/review/whole/WholeReviewFragment.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/review/whole/WholeReviewFragment.kt
@@ -1,13 +1,20 @@
 package com.nbcamp.tripgo.view.review
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.nbcamp.tripgo.R
 import com.nbcamp.tripgo.databinding.FragmentReviewBinding
 import com.nbcamp.tripgo.util.LoadingDialog
@@ -15,18 +22,34 @@ import com.nbcamp.tripgo.view.main.MainViewModel
 import com.nbcamp.tripgo.view.review.detail.ReviewDetailFragment
 import com.nbcamp.tripgo.view.reviewwriting.ReviewWritingModel
 
+
 class WholeReviewFragment : Fragment() {
     private var _binding: FragmentReviewBinding? = null
     private val binding: FragmentReviewBinding
         get() = _binding!!
     private lateinit var loadingDialog: LoadingDialog
     private lateinit var allSchedule: List<ReviewWritingModel>
+    private lateinit var categoryTitle: String
     private val sharedViewModel: MainViewModel by activityViewModels()
     private val reviewViewModel: ReviewViewModel by viewModels { ReviewViewModelFactory() }
     private val reviewAdapter: WholeReviewAdapter by lazy {
         WholeReviewAdapter(requireActivity()) { model ->
             goToReviewDetailFragment(model)
         }
+    }
+    private val bottomSheetAdapter: BottomSheetAdapter by lazy {
+        BottomSheetAdapter { category ->
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            reviewViewModel.setFilteredReview(
+                category,
+                categoryTitle
+            )
+        }
+    }
+    private val behavior by lazy {
+        BottomSheetBehavior.from(
+            binding.commentBottomSheet.root
+        )
     }
 
     override fun onCreateView(
@@ -50,7 +73,45 @@ class WholeReviewFragment : Fragment() {
             adapter = reviewAdapter
             layoutManager = LinearLayoutManager(requireActivity())
         }
+        reviewChipRegion.setOnClickListener {
+            val categoryList = resources.getStringArray(R.array.si_do)
+            runBottomSheetForFiltering(reviewChipRegion.text, categoryList)
+        }
+        reviewChipGender.setOnClickListener {
+            val categoryList = resources.getStringArray(R.array.gender)
+            runBottomSheetForFiltering(reviewChipGender.text, categoryList)
+        }
+        reviewChipAge.setOnClickListener {
+            val categoryList = resources.getStringArray(R.array.generation)
+            runBottomSheetForFiltering(reviewChipAge.text, categoryList)
+        }
+        reviewChipCompanion.setOnClickListener {
+            val categoryList = resources.getStringArray(R.array.companion)
+            runBottomSheetForFiltering(reviewChipCompanion.text, categoryList)
+        }
+        reviewDetailRestore.setOnClickListener {
+            reviewViewModel.setFilteredReview("", "")
+        }
+
         reviewViewModel.getAllReviews()
+    }
+
+    private fun runBottomSheetForFiltering(
+        text: CharSequence?,
+        categoryList: Array<String>
+    ) = with(binding) {
+
+        // 높이 조절
+        val layoutParams = commentBottomSheet.root.layoutParams
+        layoutParams.height = getBottomSheetDialogDefaultHeight()
+        commentBottomSheet.root.layoutParams = layoutParams
+        behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        categoryTitle = text.toString()
+        commentBottomSheet.reviewBottomSheetTitleTextView.text = text
+        commentBottomSheet.reviewBottomSheetRecyclerView.run {
+            adapter = bottomSheetAdapter
+        }
+        bottomSheetAdapter.addItems(categoryList.toList())
     }
 
     private fun initViewModel() = with(reviewViewModel) {
@@ -65,9 +126,31 @@ class WholeReviewFragment : Fragment() {
                     setInvisible()
                     state.message?.let { setText(it) }
                 }
+                if(state.allSchedules?.isEmpty() == true) {
+                    binding.reviewDetailNoticeTextView.run {
+                        isVisible = true
+                        text = "아직 작성 된 리뷰가 없어요.. 첫 리뷰를 작성해보세요 :)"
+                    }
+                    reviewAdapter.submitList(emptyList())
+                    return@observe
+                }
             }
+            binding.reviewDetailNoticeTextView.isVisible = false
             allSchedule = state.allSchedules ?: emptyList()
             reviewAdapter.submitList(allSchedule)
+        }
+
+        filteredList.observe(viewLifecycleOwner) { list ->
+            if(list.isEmpty()) {
+                binding.reviewDetailNoticeTextView.run {
+                    isVisible = true
+                    text = "조건에 맞는 리뷰가 존재 하지 않습니다.. :)"
+                }
+                reviewAdapter.submitList(emptyList())
+                return@observe
+            }
+            binding.reviewDetailNoticeTextView.isVisible = false
+            reviewAdapter.submitList(list)
         }
     }
 
@@ -81,6 +164,24 @@ class WholeReviewFragment : Fragment() {
         ).addToBackStack(null)
             .commit()
     }
+
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getWindowHeight() * 50 / 100
+    }
+
+    private fun getWindowHeight(): Int {
+        val wm = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = wm.currentWindowMetrics
+            val insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            windowMetrics.bounds.height() - insets.bottom - insets.top
+        } else {
+            val displayMetrics = DisplayMetrics()
+            wm.defaultDisplay.getMetrics(displayMetrics)
+            displayMetrics.heightPixels
+        }
+    }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
@@ -89,6 +190,6 @@ class WholeReviewFragment : Fragment() {
     companion object {
         fun newInstance() = WholeReviewFragment()
 
-        const val TAG = "WHOLE_REVIEW_FRAGMENT"
+        const val TAG = "REVIEW_FRAGMENT"
     }
 }
