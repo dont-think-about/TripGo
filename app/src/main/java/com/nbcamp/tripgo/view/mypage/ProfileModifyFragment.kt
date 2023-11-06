0b0cd38b3bb85081079ebec3b5ef61b75a048941
JaeMinIm <n94n@naver.com>
2023-11-07 04:06:38 +0900

    [FIX] 버그 및 부족한 기능부분 1차 수정

--- app/src/main/java/com/nbcamp/tripgo/view/mypage/ProfileModifyFragment.kt
+++ app/src/main/java/com/nbcamp/tripgo/view/mypage/ProfileModifyFragment.kt
@@ -1,20 +1,37 @@
 package com.nbcamp.tripgo.view.mypage
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.nbcamp.tripgo.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import retrofit2.http.Url
+import java.util.UUID
 
 class ProfileModifyFragment : Fragment() {
     private lateinit var refreshNickText: AppCompatEditText
+    private var selectedImageUri: Uri? = null  // 이미지 URI를 저장할 변수
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,19 +42,30 @@ class ProfileModifyFragment : Fragment() {
         refreshNickText = view.findViewById(R.id.profile_edit_username)
 
         val saveButton = view.findViewById<AppCompatImageView>(R.id.profile_edit_complete_textivew)
-        saveButton.setOnClickListener { updateNickname() }
+        saveButton.setOnClickListener { updateProfile() }
 
         val backButton = view.findViewById<AppCompatImageView>(R.id.profile_back_imagebutton)
         backButton.setOnClickListener{ navigateToMyPageFragment() }
 
+        val imageButton = view?.findViewById<ImageView>(R.id.profile_edit_user_imageview)
+        imageButton?.setOnClickListener { changeimage() }
+
+        imageupdate()
 
         return view
     }
 
-
-
-    private fun updateNickname() {
+    private fun updateProfile() {
         val editNickname = refreshNickText.text.toString()
+        if (editNickname.isEmpty()) {
+            showToast("닉네임을 입력하세요.")
+            return
+        }
+        if (selectedImageUri == null) {
+            showToast("프로필 이미지를 선택하세요.")
+            return
+        }
+
         val firestore = FirebaseFirestore.getInstance()
         val userId = userEmail()
         val userRef = firestore.collection("users").document(userId)
@@ -52,11 +80,19 @@ class ProfileModifyFragment : Fragment() {
                         if (!nicknameExists) {
                             val data = hashMapOf("nickname" to editNickname)
 
-                            userRef.update(data as Map<String, Any>).addOnSuccessListener {
-                                showToast("닉네임이 업데이트되었습니다.")
-                                navigateToMyPageFragment()
-                            }.addOnFailureListener {
-                                showToast("닉네임 업데이트에 실패했습니다.")
+                            // 이미지를 Firebase Storage에 업로드하고 업로드가 완료되면 Firestore에 이미지 URL을 업데이트합니다.
+                            uploadImageToFirebaseStorage(selectedImageUri) { imageUrl ->
+                                if (imageUrl.isNotEmpty()) {
+                                    data["profileImageUrl"] = imageUrl  // 이미지 URL을 데이터에 추가
+                                    userRef.update(data as Map<String, Any>).addOnSuccessListener {
+                                        showToast("프로필이 업데이트되었습니다.")
+                                        navigateToMyPageFragment()
+                                    }.addOnFailureListener {
+                                        showToast("프로필 업데이트에 실패했습니다.")
+                                    }
+                                } else {
+                                    showToast("프로필 이미지 업데이트에 실패했습니다.")
+                                }
                             }
                         } else {
                             showToast("닉네임이 이미 사용중입니다.")
@@ -101,5 +137,71 @@ class ProfileModifyFragment : Fragment() {
             .replace(R.id.main_fragment_container, MyPageFragment())
             .addToBackStack(null)
             .commit()
+    }
+
+    private fun changeimage() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    private fun uploadImageToFirebaseStorage(imageUri: Uri?, callback: (String) -> Unit) {
+        val userId = userEmail()
+        val storagePath = "users/$userId/profileImage/${UUID.randomUUID()}.jpg"
+        val storageReference = FirebaseStorage.getInstance().getReference(storagePath)
+
+        val uploadTask = storageReference.putFile(imageUri!!)
+
+        uploadTask.addOnSuccessListener { taskSnapshot ->
+            storageReference.downloadUrl.addOnSuccessListener { uri ->
+                val imageUrl = uri.toString()
+                callback(imageUrl)
+            }
+        }.addOnFailureListener { exception ->
+            Log.e("ProfileModifyFragment", "Storage에 업로드 오류 : $exception")
+            callback("")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            selectedImageUri = data?.data
+            val imageView = view?.findViewById<ImageView>(R.id.profile_edit_user_imageview)
+            imageView?.setImageURI(selectedImageUri)
+            Log.d("imageurl", selectedImageUri.toString())
+        }
+    }
+
+    private fun imageupdate() {
+        val firestore = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        val userEmail = user?.email
+        val userRef = firestore.collection("users").document(userEmail.toString())
+
+        userRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document != null && document.exists()) {
+                    val profileImageUrl = document.getString("profileImageUrl")
+                    if (!profileImageUrl.isNullOrEmpty()) {
+
+                        val modifyimageview = view?.findViewById<ImageView>(R.id.profile_edit_user_imageview)
+
+                        Log.d("MYpageurl", profileImageUrl)
+
+                        modifyimageview?.load(profileImageUrl){
+                            transformations(CircleCropTransformation())
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    companion object{
+        private const val GALLERY_REQUEST_CODE = 123
     }
 }
